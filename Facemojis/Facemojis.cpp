53961c00c826e51f6a795d462ccc27f8c7b959c2
charline_rochelle <charline.rochelle@gmail.com>
2017-04-18 19:27:30 +0200

    proto

--- Facemojis/Facemojis.cpp
+++ Facemojis/Facemojis.cpp
@@ -10,38 +10,81 @@
 
 cv::Mat nextInput;
 
-int video(char* videoname) {
+/// <summary>
+/// <para>L'image à utiliser.</para>
+/// </summary>
+enum Mode
+{
+	None , /// Pas d'image
+	Normal , /// Image de base
+	Smile /// Image avec un sourire
+};
+
+/// <summary>
+/// <para>Pour placer une image en surimpression de la vidéo.</para>
+/// <param name="position">Le centre du cercle à remplir avec une image.</param>
+/// <param name="angle">L'angle de rotation de l'image à incorporer.</param>
+/// <param name="radius">Le scale de l'image à effectuer.</param>
+/// <param name="mode">L'image à utiliser.</param>
+/// </summary>
+void placePicture ( cv::Mat position , float angle , float radius , Mode mode )
+{
+	// prototype
+}
+
+/// <summary>
+/// <para>Pour placer une image en surimpression de la vidéo.</para>
+/// <param name="position">Le centre du cercle à remplir avec une image.</param>
+/// <param name="angle">L'angle de rotation de l'image à incorporer.</param>
+/// <param name="mode">L'image à utiliser.</param>
+/// </summary>
+void placePiture ( cv::Mat position , float angle , Mode mode )
+{
+	placePicture ( position , angle , 10.0 , mode );
+}
+
+/// <summary>
+/// <para>Pour placer une image en surimpression de la vidéo.</para>
+/// <param name="position">Le centre du cercle à remplir avec une image.</param>
+/// <param name="mode">L'image à utiliser.</param>
+/// </summary>
+void placePiture ( cv::Mat position , Mode mode )
+{
+	placePicture ( position , 0.0 , mode );
+}
+
+int video ( char* videoname )
+{
 
 	cv::VideoCapture cap;
 
-	cap = cv::VideoCapture(1); //Camera frontale par défault de ma machine
-	if (!cap.isOpened())
-		cap = cv::VideoCapture(0);
-	
-	if (!cap.isOpened())  // check if we succeeded
+	cap = cv::VideoCapture ( 1 ); //Camera frontale par défault de ma machine
+	if ( !cap.isOpened ( ) )
+		cap = cv::VideoCapture ( 0 );
+
+	if ( !cap.isOpened ( ) )  // check if we succeeded
 		return -1;
 
 	//recuperer une image depuis cap et la stocker dans nextInput
 	cap >> nextInput;
 	//tant que nextinput n’est pas vide
-	while (!nextInput.empty())
+	while ( !nextInput.empty ( ) )
 	{
 		// - > faire les traitements sur l’image (prochaines étapes)
 
-
 		// - > appeler la fonction de dessin
-		cv::imshow("input", nextInput);
+		cv::imshow ( "input" , nextInput );
 		// - > recuperer une nouvelle image et la stocker dans nextInput
 		cap >> nextInput;
 		// - > attendre 10ms que l’utilisateur tape une touche, et quitter si il le fait
-		if (cv::waitKey(10) >= 0) break;
+		if ( cv::waitKey ( 10 ) >= 0 ) break;
 	}
 	return 0;
 }
 
-int main()
+int main ( )
 {
-	int r = video(NULL);
+	int r = video ( NULL );
 	return r;
 }
 
