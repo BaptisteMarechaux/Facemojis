da6806aac359cf6b4de8501991d710b539f30569
Pandaka <tristancailleau@hotmail.fr>
2017-04-21 10:08:51 +0200

    mise a jour

--- Facemojis/Facemojis.cpp
+++ Facemojis/Facemojis.cpp
@@ -13,12 +13,140 @@
 #include <string>
 #include <vector>
 
+const cv::String NEUTRAL_PATH = "./Emoji/neutral.png";
+const cv::String SMILE_PATH = "./Emoji/smile.png";
+const cv::String WINK_PATH = "./Emoji/wink.png";
+const cv::String SAD_PATH = "./Emoji/sad.png";
+
 cv::Mat nextInput;
 cv::Mat gray;
 std::string face_cascade_name = "haarcascade_frontalface_alt.xml";
 std::string eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
 cv::CascadeClassifier face_cascade;
 cv::CascadeClassifier eyes_cascade;
+
+
+/// <summary>
+/// <para>L'image à utiliser.</para>
+/// </summary>
+enum Mode
+{
+	NONE, /// Pas d'image
+	NEUTRAL, /// Image de base
+	SMILE, /// Image avec un sourire
+	WINK, /// Image avec cli d'oeil
+	SAD /// Image avec sad
+};
+
+/// <summary>
+/// <para>Pour placer une image en surimpression de la vidéo.</para>
+/// <param name="x">Le centre du cercle à remplir avec une image.</param>
+/// <param name="y">Le centre du cercle à remplir avec une image.</param>
+/// <param name="angle">L'angle de rotation de l'image à incorporer.</param>
+/// <param name="radius">Le scale de l'image à effectuer.</param>
+/// <param name="mode">L'image à utiliser.</param>
+/// </summary>
+void placePicture(int x, int y, float radius, Mode mode)
+{
+	// Si pas d'image à mettre en superposition osef !
+	if (mode == NONE)
+		return;
+
+	// trouver le path nécessaire
+	cv::String pathtoload;
+	switch (mode)
+	{
+	case SMILE:
+		pathtoload = SMILE_PATH;
+		break;
+	case WINK:
+		pathtoload = WINK_PATH;
+		break;
+	case SAD:
+		pathtoload = SAD_PATH;
+		break;
+	case NEUTRAL:
+	default:
+		pathtoload = NEUTRAL_PATH;
+		break;
+	}
+
+	// 1 load l'image nécessaire
+	cv::Mat img = cv::imread(pathtoload, cv::IMREAD_UNCHANGED), imgresize, imgrotate;
+	// 2 resize l'image
+	float percent = radius / img.size().height;
+	//imgresize = cv::Mat ( radius , radius , cv::IMREAD_UNCHANGED );
+	cv::resize(img, imgresize, cv::Size(), percent, percent);
+
+	int xpos, ypos;
+	int xmin = x - imgresize.size().width / 2;
+	int xmax = x + imgresize.size().width / 2;
+	if (xmin < 0)
+	{
+		xpos = 0;
+	}
+	else if (xmax > nextInput.size().width)
+	{
+		xpos = nextInput.size().width - imgresize.size().width;
+	}
+	else
+	{
+		xpos = xmin;
+	}
+
+	int ymin = y - imgresize.size().height / 2;
+	int ymax = y + imgresize.size().height / 2;
+	if (ymin < 0)
+	{
+		ypos = 0;
+	}
+	else if (ymax > nextInput.size().height)
+	{
+		ypos = nextInput.size().height - imgresize.size().height;
+	}
+	else
+	{
+		ypos = ymin;
+	}
+
+	// 4 appliquer l'image
+	cv::Rect roi(cv::Point(xpos, ypos), imgresize.size());
+	cv::Mat4b temp = imgresize;
+	cv::Mat3b dst = nextInput(roi);
+	// Pour chaque ligne
+	for (int r = 0; r < dst.rows; ++r)
+	{
+		// pour chaque colonne
+		for (int c = 0; c < dst.cols; ++c)
+		{
+			// on récupère les channels
+			const cv::Vec4b& vf = temp(r, c);
+			if (vf[3] > 0) // si alpha channel > 0
+			{
+				// Blending
+				cv::Vec3b& vb = dst(r, c);
+				vb[0] = vf[0];
+				vb[1] = vf[1];
+				vb[2] = vf[2];
+			}
+		}
+	}
+}
+
+/// <summary>
+/// <para>Pour placer une image en surimpression de la vidéo.</para>
+/// <param name="position">Le centre du cercle à remplir avec une image.</param>
+/// <param name="mode">L'image à utiliser.</param>
+/// </summary>
+void placePicture(int x, int y, Mode mode)
+{
+	placePicture(x, y, 50.0f, mode);
+}
+
+
+
+
+
 
 //cv::RNG rng(12345);
 
@@ -76,6 +204,8 @@ int video(char* videoname) {
 	{
 		// - > faire les traitements sur l’image (prochaines étapes)
 		//detectAndDisplay(nextInput);
+
+		placePicture(100, 100, 150, SMILE);
 
 		// - > appeler la fonction de dessin
 		cv::imshow("input", nextInput);
